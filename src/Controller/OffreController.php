251344d3aecf1cde8396c47fedf3bfd69d402098
TiramisuAddict <mohamedtaha.medien@gmail.com>
2026-04-05 20:15:06 +0100

    feat: Add dashboard and home page for job offers with corresponding templates

--- src/Controller/OffreController.php
+++ src/Controller/OffreController.php
@@ -21,6 +21,15 @@ final class OffreController extends AbstractController
     }
     
     //Offres
+    #[Route('/offre/home', name: 'app_offre_home')]
+    public function home(OffreRepository $offre_repository){
+        $offres = $offre_repository->findAll();
+        return $this->render('offre/home_page.html.twig' , [ //page
+            'offres' => $offres
+        ]);
+    }
+
+    //Liste des offres
     #[Route('/offre/list', name: 'app_offre_list')]
     public function listOffres(OffreRepository $offre_repository){
         $offres = $offre_repository->findAll();
@@ -29,11 +38,11 @@ final class OffreController extends AbstractController
         ]);
     }
 
-    //Offres
-    #[Route('/offre/home', name: 'app_offre_home')]
-    public function home(OffreRepository $offre_repository){
+    //dashboard_offre_hr
+    #[Route('/offre/dashboard', name: 'app_offre_dashboard')]
+    public function dashboard(OffreRepository $offre_repository){
         $offres = $offre_repository->findAll();
-        return $this->render('offre/home_page.html.twig' , [ //page
+        return $this->render('offre/dashboard_offre_hr.html.twig' , [ //
             'offres' => $offres
         ]);
     }
