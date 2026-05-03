5ba1c4fe4fb1e8b6e4a679f8a5ff919ad2135234
arfaoui ghada <arfaouighada15@gmail.com>
2026-05-03 21:47:22 +0100

    correction visiteur

--- src/Entity/Visiteur.php
+++ src/Entity/Visiteur.php
@@ -139,9 +139,5 @@ class Visiteur implements PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function __construct()
-    {
-        $this->candidats = new ArrayCollection();
-    }
 
 }
