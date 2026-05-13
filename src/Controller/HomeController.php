9b0a8fbbdf19242049910acb3e1c468d6b5f2dd7
Dahen Baheeddine <dahen.baheeddine@gmail.com>
2026-05-13 21:42:21 +0100

    fix des erreurs de navigation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,31 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
-        return $this->redirectToRoute('app_formation_rh');
+        if ($session->get('admin_logged_in') === true) {
+            return $this->redirectToRoute('app_admin_dashboard');
+        }
+
+        if ($session->get('employe_logged_in') === true) {
+            $role = mb_strtolower(trim((string) $session->get('employe_role', '')));
+
+            return match ($role) {
+                'administrateur entreprise', 'rh' => $this->redirectToRoute('app_admin_dashboard'),
+                default => $this->redirectToRoute('employe_Home'),
+            };
+        }
+
+        if ($session->get('visiteur_logged_in') === true) {
+            return $this->redirectToRoute('app_offre_home');
+        }
+
+        return $this->redirectToRoute('app_offre_home');
     }
 }
